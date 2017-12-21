2cef638a47cfd099b574abad5a2de38478ad4112
Luiza Guerra <luiza.guerra@gmail.com>
2017-12-21 10:02:11 -0200

    using toBeNull

---
+++ spec/toBeNull.js
@@ -0,0 +1,15 @@
+describe("Comparador toBeNull", function(){
+    it("deve validar o uso do toBeNull", function(){
+        var n1 = null;
+        var n2 = undefined;
+        var n3;
+        var n4 = NaN;
+        var n5 = "teste";
+        expect(n1).toBeNull();
+        expect(n2).not.toBeNull();
+        expect(n3).not.toBeNull();
+        expect(n4).not.toBeNull();
+        expect(n5).not.toBeNull();
+
+    });
+});
